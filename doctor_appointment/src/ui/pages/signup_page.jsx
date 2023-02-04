b608a4263ea94676a24813bc1116ff27aba3ca54
Mohamed Safwan <Mohamedsafwan220@gmail.com>
2023-02-04 16:56:35 +0530

    added doctor auth

--- doctor_appointment/src/ui/pages/signup_page.jsx
+++ doctor_appointment/src/ui/pages/signup_page.jsx
@@ -1,66 +1,108 @@
 import { Button, TextField } from "@mui/material";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { addDoc, collection } from "firebase/firestore";
 import React, { useState } from "react";
-import { auth } from "../../../../doctor_appointment_patient/src/config/firebase";
-import { LOGIN_PAGE_ROUTE } from "../../config/routes";
+import { useNavigate } from "react-router-dom";
+import { auth, db } from "../../config/firebase";
+import { HOME_PAGE_ROUTE, LOGIN_PAGE_ROUTE } from "../../config/routes";
 
 function SignupPage() {
+  const navigate = useNavigate();
+
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [error, setError] = useState("");
+  const [department, setDepartment] = useState("");
+  const [fees, setFees] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  const validatePassword = () => {
-    let isValid = true;
-    if (password !== "" && confirmPassword !== "") {
-      if (password !== confirmPassword) {
-        isValid = false;
-        setError("Passwords does not match");
-      }
-    }
-    return isValid;
-  };
+  const register = async (e) => {
+    setIsLoading(true);
 
-  const register = (e) => {
     e.preventDefault();
-    setError("");
-    if (validatePassword()) {
-      // Create a new user with email and password using firebase
-      createUserWithEmailAndPassword(auth, email, password)
-        .then(() => {
-          console.log(
-            "user created with email:" + email + "password" + password
-          );
-        })
-        .catch((err) => setError(err.message));
+
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      await updateProfile(res.user, {
+        displayName: name,
+      });
+
+      const doctorsRef = collection(db, "doctors");
+      await addDoc(doctorsRef, {
+        email,
+        password,
+        department,
+        fees,
+      });
+
+      localStorage.setItem("user", res.user);
+
+      setIsLoading(false);
+
+      navigate(HOME_PAGE_ROUTE);
+    } catch (e) {
+      setIsLoading(false);
     }
-    setEmail("");
-    setPassword("");
-    setConfirmPassword("");
   };
-  console.log(email);
 
   return (
     <main className="bg-disabled min-h-screen flex justify-center items-center">
       <div className="rounded-xl bg-white w-[40%] flex justify-center items-center flex-col space-y-4 p-16">
-        <div className="w-[100%] space-y-4">
+        <form className="w-[100%] space-y-4" onSubmit={register}>
           <h1 className="font-[500] text-primary text-3xl">Create Account</h1>
           <h1 className="text-secondary mt-2">Sign up for new account</h1>
           <div className="space-y-1 flex flex-col">
             <label htmlFor="fullName" className="text-sm font-semibold">
               Full Name
             </label>
-            <TextField id="fullName" variant="outlined" size="small" />
+            <TextField
+              id="fullName"
+              variant="outlined"
+              size="small"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
           </div>
           <div className="space-y-1 flex flex-col">
             <label htmlFor="email" className="text-sm font-semibold">
-              Email or Phone
+              Email
             </label>
             <TextField
               id="email"
               variant="outlined"
               type="email"
               size="small"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+          <div className="space-y-1 flex flex-col">
+            <label htmlFor="department" className="text-sm font-semibold">
+              Department
+            </label>
+            <TextField
+              id="department"
+              variant="outlined"
+              size="small"
+              onChange={(e) => {
+                setDepartment(e.target.value);
+              }}
+            />
+          </div>
+          <div className="space-y-1 flex flex-col">
+            <label htmlFor="fees" className="text-sm font-semibold">
+              Fees
+            </label>
+            <TextField
+              id="fees"
+              variant="outlined"
+              size="small"
+              onChange={(e) => {
+                setFees(e.target.value);
+              }}
             />
           </div>
           <div className="space-y-1 flex flex-col">
@@ -72,6 +114,9 @@ function SignupPage() {
               variant="outlined"
               type="password"
               size="small"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
             />
           </div>
           <div className="space-y-1 flex flex-col">
@@ -85,8 +130,13 @@ function SignupPage() {
               size="small"
             />
           </div>
-          <Button variant="contained" className="w-full">
-            Sign up
+          <Button
+            variant="contained"
+            className="w-full"
+            type="submit"
+            disabled={isLoading}
+          >
+            {isLoading ? "Loading.." : "Sign up"}
           </Button>
           <div className="text-center">
             <p className="text-secondary text-[0.86em]">
@@ -96,7 +146,7 @@ function SignupPage() {
               </Button>
             </p>
           </div>
-        </div>
+        </form>
       </div>
     </main>
   );
